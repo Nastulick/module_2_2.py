5d3086290523010a0afaab6405347d64be0af660
Nastulick <nastya9031976011@gmail.com>
2024-12-07 18:30:35 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 213
+second = 693
+third = 478
+if first == second and second == third :
+    print(3)
+elif first == second != third :
+    print(2)
+elif first != second != third :
+    print(0)
+
